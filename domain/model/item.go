645a159421a2504a3a810794397ef95f2ba172b3
Oishi Akito <66ed3gs@gmail.com>
2021-02-26 16:11:57 +0900

    refactor(model): create new struct

--- domain/model/item.go
+++ domain/model/item.go
@@ -2,11 +2,20 @@ package model
 
 // Item - item
 type Item struct {
-	ID            int    `json:"id"`
-	Title         string `json:"title"`
-	ThumbnailURL  string `json:"thumbnailUrl"`
-	MusicTitle    string `json:"musicTitle"`
-	Rating        int    `json:"rating"`
-	Price         int    `json:"price"`
-	DownloadCount int    `json:"downloadCount"`
+	ID         int            `firestore:"id" json:"id"`
+	Snippet    ItemSnippet    `firestore:"snippet" json:"snippet"`
+	Statistics ItemStatistics `firestore:"statistics" json:"statistics"`
+}
+
+type ItemSnippet struct {
+	Title        string   `firestore:"title" json:"title"`
+	ThumbnailURL string   `firestore:"thumbnailUrl" json:"thumbnailUrl"`
+	MusicTitle   string   `firestore:"musicTitle" json:"musicTitle"`
+	Price        int      `firestore:"price" json:"price"`
+	Tags         []string `firestore:"tags" json:"tags"`
+}
+
+type ItemStatistics struct {
+	Rating        float64 `firestore:"rating" json:"rating"`
+	DownloadCount int     `firestore:"downloadCount" json:"downloadCount"`
 }
