203fb67c1c719ab67bf1c17cda64cf43e4203dfe
Oishi Akito <66ed3gs@gmail.com>
2021-02-28 23:20:19 +0900

    add(pkg): create `random` package

---
+++ pkg/random/string.go
@@ -0,0 +1,14 @@
+package random
+
+import "math/rand"
+
+// String - generate random string
+func String(n int) string {
+	var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+	return string(b)
+}
