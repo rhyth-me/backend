032657d000db4e36a392df41bfaff2facb628b54
Oishi Akito <66ed3gs@gmail.com>
2021-03-02 16:38:25 +0900

    change(model): add omitempty in `item`

--- domain/model/item.go
+++ domain/model/item.go
@@ -3,20 +3,20 @@ package model
 // Item - item
 type Item struct {
 	ID         string         `firestore:"id" json:"id"`
-	Snippet    ItemSnippet    `firestore:"snippet" json:"snippet"`
-	Statistics ItemStatistics `firestore:"statistics" json:"statistics"`
-	Author     SocialProfile  `firestore:"author" json:"author"`
+	Snippet    ItemSnippet    `firestore:"snippet" json:"snippet,omitempty"`
+	Statistics ItemStatistics `firestore:"statistics" json:"statistics,omitempty"`
+	Author     SocialProfile  `firestore:"author" json:"author,omitempty"`
 }
 
 type ItemSnippet struct {
-	Title        string   `firestore:"title" json:"title"`
-	ThumbnailURL string   `firestore:"thumbnailUrl" json:"thumbnailUrl"`
-	MusicTitle   string   `firestore:"musicTitle" json:"musicTitle"`
-	Price        int      `firestore:"price" json:"price"`
-	Tags         []string `firestore:"tags" json:"tags"`
+	Title        string   `firestore:"title" json:"title,omitempty"`
+	ThumbnailURL string   `firestore:"thumbnailUrl" json:"thumbnailUrl,omitempty"`
+	MusicTitle   string   `firestore:"musicTitle" json:"musicTitle,omitempty"`
+	Price        int      `firestore:"price" json:"price,omitempty"`
+	Tags         []string `firestore:"tags" json:"tags,omitempty"`
 }
 
 type ItemStatistics struct {
-	Rating        float64 `firestore:"rating" json:"rating"`
-	DownloadCount int     `firestore:"downloadCount" json:"downloadCount"`
+	Rating        float64 `firestore:"rating" json:"rating,omitempty"`
+	DownloadCount int     `firestore:"downloadCount" json:"downloadCount,omitempty"`
 }
