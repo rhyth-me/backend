df65154b6056dee01245ac291ffdcbf83ea682f1
Oishi Akito <66ed3gs@gmail.com>
2021-03-06 11:54:27 +0900

    add(pkg): create `authority`

---
+++ pkg/authority/types.go
@@ -0,0 +1,22 @@
+package authority
+
+import "github.com/labstack/echo/v4"
+
+// CustomContext - echoContext expansion
+type CustomContext struct {
+	echo.Context
+	User User
+}
+
+// User - The identifier of the authenticated user.
+type User struct {
+	UID        string
+	ScreenName string
+	Google     Google
+}
+
+// Google - Authenticated user's account.
+type Google struct {
+	ID    string
+	Email string
+}
