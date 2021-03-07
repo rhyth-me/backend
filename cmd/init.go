5035b4d084892f611a7bbe186eadc8490af9fe12
Oishi Akito <66ed3gs@gmail.com>
2021-03-08 01:18:20 +0900

    change(cmd): move to api_gen v2

---
+++ cmd/init.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/rhyth-me/backend/api/apigen/props"
+	"github.com/rhyth-me/backend/pkg/firebase/auth"
+	"github.com/rhyth-me/backend/pkg/validator"
+)
+
+// initControllerProps - setup controller props
+func initControllerProps() *props.ControllerProps {
+	p := new(props.ControllerProps)
+
+	return p
+}
+
+// initEchoSetting - setup echo
+func initEchoSetting(e *echo.Echo) *echo.Echo {
+	e.Debug = true
+	e.HideBanner = true
+	e.Validator = validator.NewValidator()
+	e.Use(middleware.Recover())
+
+	// Custom context for identifying users
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := auth.Identify(c)
+			return next(cc)
+		}
+	})
+
+	// CORS config
+	origins := []string{"https://rhyth.me"}
+	if os.Getenv("STAGING") == "true" {
+		origins = []string{"*"}
+	}
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: origins,
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
+
+	return e
+}
--- cmd/main.go
+++ cmd/main.go
@@ -2,76 +2,35 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
-	"strings"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"github.com/rhyth-me/backend/interfaces"
-	"github.com/rhyth-me/backend/interfaces/props"
-	"github.com/rhyth-me/backend/pkg/authority"
-	"github.com/rhyth-me/backend/pkg/firebase"
-	"github.com/rhyth-me/backend/pkg/validator"
-	"github.com/stripe/stripe-go/v72/client"
+	"github.com/rhyth-me/backend/api/apigen"
+	"github.com/rhyth-me/backend/pkg/firebase/auth"
+	"github.com/rhyth-me/backend/pkg/firebase/firestore"
 )
-
-func initEchoSetting(e *echo.Echo) {
-	e.Debug = true
-	e.HideBanner = true
-	e.Validator = validator.NewValidator()
-	e.Pre(middleware.AddTrailingSlash())
-	e.Use(middleware.Recover())
-
-	// Firebase auth - check login user
-	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			cc := authority.Identify(c, c.Request().Header.Get("X-Token"))
-			return next(cc)
-		}
-	})
-
-	// CORS config
-	origins := []string{"https://rhyth.me"}
-	if os.Getenv("STAGING") == "true" {
-		origins = []string{"*"}
-	}
-
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: origins,
-		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
-	}))
-}
-
-func initControllerProps() *props.ControllerProps {
-
-	p := new(props.ControllerProps)
-
-	// firebase init
-	p.Firestore = firebase.InitFirestore()
-	p.Auth = firebase.InitAuth()
-
-	// stripe init
-	sc := &client.API{}
-	sc.Init(os.Getenv("STRIPE_API_KEY"), nil)
-	p.Stripe = sc
-
-	return p
-}
 
 func main() {
 	godotenv.Load(".env")
 
+	// Init firebase
+	auth.Init()
+	firestore.Init()
+
+	// Init echo
 	e := echo.New()
-	initEchoSetting(e)
+	e = initEchoSetting(e)
 
-	props := initControllerProps()
-	interfaces.Bootstrap(props, e, nil, os.Stdout)
+	// Init ControllerProps
+	p := initControllerProps()
 
+	apigen.Bootstrap(p, e, nil)
+
+	//
 	fmt.Println("All endpoints are...")
 	for _, r := range e.Routes() {
-		fmt.Printf("%s %s\n", r.Method, strings.TrimRight(r.Path, "/"))
+		fmt.Printf("%s %s\n", r.Method, r.Path)
 	}
 
 	if err := e.Start(":" + os.Getenv("PORT")); err != nil {
