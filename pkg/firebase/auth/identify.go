8bd97cc77fd0ccac64d21881235a531abf3361a2
Oishi Akito <66ed3gs@gmail.com>
2021-03-08 01:12:26 +0900

    add(pkg): create auth in `firebase`

---
+++ pkg/firebase/auth/identify.go
@@ -0,0 +1,56 @@
+package auth
+
+import (
+	"context"
+	"errors"
+
+	"github.com/labstack/echo/v4"
+	"github.com/rhyth-me/backend/domain/model"
+)
+
+// Identify - Identify the user from JWT.
+func Identify(c echo.Context) *CustomContext {
+	var jwt string = c.Request().Header.Get("X-Token")
+	ctx := context.Background()
+
+	token, err := Client.VerifyIDToken(ctx, jwt)
+	if err != nil {
+		return &CustomContext{Context: c}
+	}
+
+	claims := token.Claims
+	identities := claims["firebase"].(map[string]interface{})["identities"].(map[string]interface{})
+
+	var screenName string = ""
+	if val, ok := claims["screen_name"].(string); ok {
+		screenName = val
+	}
+
+	cc := &CustomContext{Context: c, User: LoginUser{
+		UID:        token.UID,
+		ScreenName: screenName,
+		Google: model.Google{
+			ID:    identities["google.com"].([]interface{})[0].(string),
+			Email: identities["email"].([]interface{})[0].(string),
+		},
+	}}
+
+	return cc
+}
+
+// IsAuthedUser - Verify if the user is logged in.
+func IsAuthedUser(c echo.Context) error {
+	au := c.(*CustomContext).User
+
+	if au.UID == "" {
+		return errors.New("authentication is required")
+	}
+	return nil
+}
+
+// GetAuthedUser - Return the identity of auth user.
+func GetAuthedUser(c echo.Context) LoginUser {
+	au := c.(*CustomContext).User
+
+	return au
+}
---
+++ pkg/firebase/auth/init.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"context"
+
+	"firebase.google.com/go/auth"
+	"github.com/golang/glog"
+	"github.com/rhyth-me/backend/pkg/firebase"
+)
+
+// Client - firebase authentication client
+var Client *auth.Client
+
+// Init - setup firebase authentication client
+func Init() *auth.Client {
+	ctx := context.Background()
+	app := firebase.Init()
+
+	cli, err := app.Auth(ctx)
+	if err != nil {
+		glog.Errorln(err)
+	}
+
+	Client = cli
+
+	return cli
+}
