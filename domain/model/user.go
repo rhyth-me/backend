3a22d9b1ff35741de57e790a4e5e5a91765688e8
Oishi Akito <66ed3gs@gmail.com>
2021-03-08 01:16:22 +0900

    change(model): chnage user model

--- domain/model/user.go
+++ domain/model/user.go
@@ -1,23 +1,27 @@
 package model
 
-import "github.com/rhyth-me/backend/pkg/authority"
-
 // User - users collection structure.
 type User struct {
-	UID     string           `firestore:"uid" json:"-"`
-	Google  authority.Google `firestore:"google" json:"-"`
-	Profile SocialProfile    `firestore:"profile" json:"profile,omitempty"`
-	Payout  PayoutSettings   `firestore:"payout" json:"-"`
+	UID     string         `firestore:"uid" json:"-"`
+	Google  Google         `firestore:"google" json:"-"`
+	Profile SocialProfile  `firestore:"profile" json:"profile,omitempty"`
+	Payout  PayoutSettings `firestore:"payout" json:"-"`
 }
 
 // Field Structures
 
+// Google - The user's Google account.
+type Google struct {
+	ID    string `firestore:"id" json:"-"`
+	Email string `firestore:"email"  json:"-"`
+}
+
 // SocialProfile - details of the user as displayed on the site.
 type SocialProfile struct {
-	ScreenName       string `firestore:"screenName" json:"screenName,omitempty" validate:"gt=0,max=20"`
-	DisplayName      string `firestore:"displayName" json:"displayName,omitempty" validate:"gt=0,max=20"`
-	ProfileImagePath string `firestore:"profileImagePath" json:"profileImagePath,omitempty" validate:"required"`
-	StatusMessage    string `firestore:"statusMessage" json:"statusMessage,omitempty" validate:"min=0,max=150"`
+	ScreenName       string `firestore:"screenName" json:"screenName" validate:"gt=0,max=25"`
+	DisplayName      string `firestore:"displayName" json:"displayName" validate:"gt=0,max=20"`
+	ProfileImagePath string `firestore:"profileImagePath" json:"profileImagePath" validate:"required"`
+	StatusMessage    string `firestore:"statusMessage" json:"statusMessage" validate:"min=0,max=150"`
 }
 
 // PayoutSettings - information for withdrawal.
