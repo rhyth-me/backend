2dec5c170d03082d15c6810e118f435aa09c5bfd
Oishi Akito <66ed3gs@gmail.com>
2021-03-10 09:16:27 +0900

    add(api): create GetCards

--- api/account/cards.go
+++ api/account/cards.go
@@ -2,6 +2,16 @@ package account
 
 import "github.com/rhyth-me/backend/domain/model"
 
+// GetCardsRequest - Fetch the list of card information.
+type GetCardsRequest struct{}
+
+// GetCardsResponse - Return the list of card information.
+type GetCardsResponse struct {
+	Code    int          `json:"code"`
+	Message string       `json:"message,omitempty"`
+	Result  []model.Card `json:"result,omitempty"`
+}
+
 // PutCardsRequest - Create a new card in Stripe
 type PutCardsRequest struct {
 	Token string `json:"token"`
