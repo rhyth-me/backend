d5c4b6d55eca5fc330c60cefffba512a435b2ef8
Oishi Akito <66ed3gs@gmail.com>
2021-03-13 19:37:35 +0900

    add(api): add DeleteImage in account

--- api/account/patch_profile_controller_gen.go
+++ api/account/patch_profile_controller_gen.go
@@ -101,6 +101,10 @@ func (p *PatchProfileController) PatchProfile(
 				return nil, wrapper.NewAPIError(http.StatusInternalServerError)
 			}
 
+			if user.Profile.ImageHash != "" {
+				_ = storage.DeleteImage(value)
+			}
+
 			user.Profile.ImageHash = value
 		}
 	}
